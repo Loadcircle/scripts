3ecec064d60f09b72738cbdeac4725b8dc2ad003
Loadcircle <37840489+Loadcircle@users.noreply.github.com>
2020-02-12 12:17:58 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -367,3 +367,21 @@ if($(window).width() <= 768){
     {% endfor %}
     
 #}
+
+
+Counter-------------------------------------
+    $('.count-this').each(function () {
+    
+	// Start the counting from a specified number - in this case, 0!
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+      }, {
+        // Speed of counter in ms, default animation style
+          duration: 2000,
+          easing: 'swing',
+          step: function (now) {
+            // Round up the number
+              $(this).text(Math.ceil(now));
+          }
+      });
+    });
